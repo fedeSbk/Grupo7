c5b8e336d0fab354367b9a5157b165fca034a922
Federico Jara <fedejara_nqn@hotmail.com>
2019-05-03 01:18:12 -0300

    armado de cajas y primeras pantallas

---
+++ Site/hospedajes/Index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home Switch Home Site</title>
+    <!--<link rel="shortcut icon" href="favico.ico"/>-->
+    <link type="text/css" rel="stylesheet" href="../style.css" media="all">
+    </head>
+  <body>
+    <div id="contenedor">
+      <div id="encabezado">
+        <h2>Navigation Menu</h2>
+        <p> Bienvenidos a nuestro sitio online </p>
+        <div id="menu">
+          <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="login/logIn.php">Iniciar Sesion</a></li>
+            <li><a href="login/singIn.php">Registrarse</a></li>
+            <li><a href="Alta.php">Alta Hospedajes</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="pie">
+        <p> Desarrollado por <strong>FAA</strong> - 2019 - copyrigth</p>
+      </div>
+    </div>
+  </body>
+</html>
