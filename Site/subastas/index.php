f312aea5cd318f88b4b3e1c6dc593902f2bfec77
averge <48928310+averge@users.noreply.github.com>
2019-05-07 10:35:36 -0300

    agregado de subasta

---
+++ Site/subastas/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Home Switch Home Site</title>
+    <!--<link rel="shortcut icon" href="favico.ico"/>-->
+    <link type="text/css" rel="stylesheet" href="style.css" media="all">
+    </head>
+  <body>
+    <div id="contenedor">
+      <div id="encabezado">
+        <h2>Home Switch Home Site</h2>
+        <p> Bienvenidos a nuestro sitio online </p>
+        <div id="menu">
+          <ul>
+            <li><a href="../index.php">Home</a></li>
+            <li><a href="nueva_subasta.php">crear subasta</a></li>
+            <li><a href="lista_subastas.php">lista de subastas</a></li>
+          </ul>
+        </div>
+      </div>
+      <div id="condenido">
+      </div>
+      <div id="pie">
+        <p> Desarrollado por <strong>FAA</strong> - 2019 - copyrigth</p>
+      </div>
+    </div>
+  </body>
+</html>
